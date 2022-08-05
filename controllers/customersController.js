09c8cdf44dfd9712b839038b0354d340219c0624
Peyman Ahmadi <peyman.ahmadi@outlook.com>
2022-08-05 14:04:21 +0430

    Fixed create customer

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -1,6 +1,7 @@
 import Customer from "../models/Customer.js";
 import mongoose from "mongoose";
 import connectDB from "../db/connect.js";
+import { BadRequestError } from "../errors/index.js";
 
 const getAllCustomers = async (req, res) => {
   console.log("Get All Customers");
@@ -13,21 +14,32 @@ const getAllCustomers = async (req, res) => {
   }
 };
 
-const createCustomer = async (req, res) => {
-  console.log("Create Customer");
+const createCustomer = async (req, res, next) => {
+  const { name, codeName } = req.body;
+
+  if (!name || !codeName) {
+    const error = new BadRequestError("Please provide all required values");
+    return next(error);
+  }
+
+  const nameAlreadyExists = await Customer.findOne({ name });
+  const codeNameAlreadyExists = await Customer.findOne({ codeName });
+  if (nameAlreadyExists || codeNameAlreadyExists) {
+    const error = new BadRequestError("customer name/codeName already exists");
+    return next(error);
+  }
+
   try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
     let customerdata = req.body;
-    const customer = await Customer.create(customerdata);
-    console.log(customer);
+    const customer = await Customer.create([customerdata], { session: sess });
     const url = process.env.RAW_MONGO_URL.replace(
       "batching",
-      `batching_${customerdata.name}`
+      `batching_${customerdata.codeName}`
     );
-    console.log(url);
     const conn = mongoose.createConnection(url);
-    console.log(conn);
-    const userSchema = new mongoose.Schema({ name: String, email: String });
-    const UserModel = conn.model("User", userSchema);
+    sess.commitTransaction();
     res.status(201).json({ customer });
   } catch (error) {
     console.log(error);
