8d0217a0029616265027e04e3129392cb10c4e9a
Peyman Ahmadi <peyman.ahmadi@outlook.com>
2022-08-09 18:21:29 +0430

    Update login form: basic structure

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,46 @@
+import { useState, useEffect } from "react";
+import { Logo } from "../components";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
 const Login = () => {
-  return <h1>Login</h1>;
+  const [values, setValues] = useState(initialState);
+  // global state and useNavigate
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  return (
+    <section className="login-page full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            email
+          </label>
+          <input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          login
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
